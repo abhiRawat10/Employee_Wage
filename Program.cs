ebe4c2d1c02949c419d47e99458cb1a2aad0b3d9
abhirawat10 <abhi10rawat2002@gmail.com>
2025-02-21 11:49:41 +0530

    initial commit for uc-2

--- Program.cs
+++ Program.cs
@@ -3,19 +3,29 @@
     internal class Program
     {
         static void Main(string[] args)
+
+
         {
+            //constants
             int IS_FULL_TIME = 1;
             Console.WriteLine("Enter 1 for present and 0 for absent");
+            int EMP_RATE_PER_HOUR = 20;
+
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
 
             int empCheck = Convert.ToInt32(Console.ReadLine());
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage: " + empWage);
         }
     }
 }
