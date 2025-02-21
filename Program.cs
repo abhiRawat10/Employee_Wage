ad1d5f2390fa3556ed41d01d8d48c6ddbc1d2316
abhirawat10 <abhi10rawat2002@gmail.com>
2025-02-21 11:12:20 +0530

    initial commit on c-1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int IS_FULL_TIME = 1;
+            Console.WriteLine("Enter 1 for present and 0 for absent");
+
+            int empCheck = Convert.ToInt32(Console.ReadLine());
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
