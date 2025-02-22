75642c7227ef9e8fc5ec7cdaf52fb17b9b23a3af
abhirawat10 <abhi10rawat2002@gmail.com>
2025-02-22 10:55:49 +0530

    initial commit

--- Program.cs
+++ Program.cs
@@ -2,9 +2,45 @@
 {
     internal class Program
     {
+        //constants
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            //variables
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+
+            //computations
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            {
+                totalWorkingDays++;
+
+                Console.WriteLine("Welcome to Employee Wage Application");
+                Console.WriteLine("Click 0 if Absent ,Click 1 if part time , Click 2 for full time");
+                int empCheck = Convert.ToInt32(Console.ReadLine());
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#: " + totalWorkingDays + " Emp Hrs: " + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+        } 
     }
 }
