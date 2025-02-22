2f1812bd491d6f6ae6ec0a5283505dac21463c27
abhirawat10 <abhi10rawat2002@gmail.com>
2025-02-22 10:59:41 +0530

    updating some constants

--- Program.cs
+++ Program.cs
@@ -2,6 +2,10 @@
 {
     internal class Program
     {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
             //variables
@@ -10,7 +14,7 @@
             int totalEmpWage = 0;
 
             //computations for 20 days
-            for(int day=0;day < 20; day++)
+            for(int day=0;day < NUM_OF_WORKING_DAYS; day++)
             {
                 Console.WriteLine("Welcome to Employee Wage Application");
                 Console.WriteLine("Click 0 if Absent ,Click 1 if part time , Click 2 for full time");
@@ -18,17 +22,17 @@
 
                 switch (empCheck)
                 {
-                    case 1:
+                    case IS_FULL_TIME:
                         empHrs = 8;
                         break;
-                    case 2:
+                    case IS_PART_TIME:
                         empHrs = 4;
                         break;
                     default:
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * 20;//for 1 day
+                empWage = empHrs * EMP_RATE_PER_HOUR;//for 1 day
                 totalEmpWage += empWage;
                 Console.WriteLine("Emp Wage: " + empWage);
             }
